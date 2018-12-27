-fixed the normalization error in the function 'normalize' within the primitive file

models/Decomposer.py
@@ -76,6 +76,7 @@ class Decomposer(nn.Module):
         for ind in range(len(self.encoder)):
             x = self.encoder[ind](x)
             x = F.leaky_relu(x)
+            ## store intermedia values for residuel layers
             encoded.append(x)
 
         ## decode lights
@@ -91,13 +92,14 @@ class Decomposer(nn.Module):
         reflectance = self.__decode(self.decoder_reflectance, encoded, x)
         normals = self.__decode(self.decoder_normals, encoded, x)
         depth = self.__decode(self.decoder_depth, encoded, x)
-
+        print("before clamped:",normals.shape)
         ## R, G in [-1,1]
         rg = torch.clamp(normals[:,:-1,:,:], -1, 1)
         ## B in [0,1]
         b = torch.clamp(normals[:,-1,:,:].unsqueeze(1), 0, 1)
         clamped = torch.cat((rg, b), 1)
         ## normals are unit vector
+        print("before calling norm:",clamped.shape)
         normed = normalize(clamped)
 
         ## turn float mask into bool array

models/primitives.py
@@ -33,11 +33,26 @@ def decode(encoder, decoder, upsampler):
 
         return x
     return forward
+##sum 1 shape
+#norm torch.Size([4, 3, 256, 256])
+#first torch.Size([4, 256, 256])
+#second torch.Size([1, 12, 256, 256])
+
+#sum 0 shape
+#norm torch.Size([4, 3, 256, 256])
+#first torch.Size([3, 256, 256])
+#second torch.Size([1, 9, 256, 256])
 
 ## normalize to unit vectors
 def normalize(normals):
-    magnitude = torch.pow(normals, 2).sum(1)
+    print("within function normalize:")
+    print("norm",normals.shape)
+    ## I make the keepdim params to be True (default is false) so the dimension of the..
+    ## Magnitude is the same as the normals to be normed
+    magnitude = torch.pow(normals, 2).sum(1,keepdim = True)
+    print("first",magnitude.shape)
     magnitude = magnitude.sqrt().repeat(1,3,1,1)
+    print("second",magnitude.shape)
     normed = normals / (magnitude + 1e-6)
     return normed
 